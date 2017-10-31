68b22cdc80ebb27bd88ac7b2a4f14300e37f925f
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-01 01:10:34 +1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+
+<head>
+  <title>Comic Royale</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+<div class="container" name="content">
+
+<!-- Login and Registration Buttons/Links -->
+<div class="container"
+<nav>
+    <ul class="nav navbar-nav navbar-right">
+	<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
+	<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+</nav>
+	</div>
+	
+<!-- NavBar -->	
+<div class="container">
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Comic Royale</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Shop</a></li>
+      <li><a href="#">Sponsors</a></li>
+	  <li><a href="#">Contact Us</a></li>
+    </ul>
+	</div>
+	</nav>
+</div>
+<!-- Search Bar -->
+
+<form class="form-inline" role="form">
+	 <div class="form-group">
+      <label class="search">Search:</label>
+      <input type="search" class="form-control" id="ssearch placeholder="Search Comic Royale">
+    </div>
+</form>
+
+<!-- Banner with logo -->
+
+
+
+<!-- Genre links -->
+
+
+
+<!-- Carousel -->
+
+
+  
+<!-- Table of Most Popular -->
+
+
+
+<!-- Footer -->
+
+
+
+<!-- Pagination -->
+
+
+</div> <!-- end of content container -->
+
+</body>
+</html>
