de4f81740c324bf7b98def013a97cf636819caf0
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-01 20:59:06 +1100

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+  <title>Comic Royale</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+
+
 <div class="container" name="loginContainer">
  
  <!-- login form; validate each field using regEx onSubmit-->
@@ -32,3 +49,6 @@
  </form>
   
 </div>
+	
+</body>
+</html>
