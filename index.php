c02685c28a22db5df290c8459aaa74eeddb4cbc7
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-09 15:37:23 +1100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <title>Comic Royale</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="comicRoyale.css">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -16,65 +17,208 @@
 <div class="container" name="content">
 
 <!-- Login and Registration Buttons/Links -->
-<div class="container">
+<div class="row">
 <nav>
     <ul class="nav navbar-nav navbar-right">
-	<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
-	<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	<li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
+	<li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
 </nav>
-	</div>
+
 	
 <!-- NavBar -->	
 <div class="container">
 <nav class="navbar">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Comic Royale</a>
+      <a class="navbar-brand" href="index.php">Comic Royale</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Gallery</a></li>
-      <li><a href="#">Shop</a></li>
-      <li><a href="#">Sponsors</a></li>
-	  <li><a href="#">Contact Us</a></li>
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="gallery.html">Gallery</a></li>
+      <li><a href="shop.html">Shop</a></li>
+      <li><a href="sponsors.html">Sponsors</a></li>
+	  <li><a href="contact.html">Contact Us</a></li>
     </ul>
 	</div>
 	</nav>
 </div>
+</div>
+
 <!-- Search Bar -->
 
+<div class="container">
+<div class="row">
+<
 <form class="form-inline" role="form">
 	 <div class="form-group">
-      <label class="search">Search:</label>
-      <input type="search" class="form-control" id="ssearch placeholder="Search Comic Royale">
+      <label class="search">Search Comic Royale:</label>
+      <input type="search" class="form-control" id="searchPlaceholder" placeholder="What would you like to look for?">
     </div>
 </form>
+</div>
+</div><!-- end of Search bar container -->
+
+
 
 <!-- Banner with logo -->
-
-
+<div class="container">
+<a href="index.php"><img src="Images/logo.jpg"/></a>
+<a href="index.php"><img src="Images/banner.png"/></a>
+</div>
 
 <!-- Genre links -->
+
+<div class="container">
+  <a href="#"><button type="button">Horror</button></a>
+  <a href="#"><button type="button">Action</button></a>
+  <a href="#"><button type="button">Comedy</button></a>
+  <a href="#"><button type="button">Drama</button></a>
+  <a href="#"><button type="button">Sci-Fi</button></a>
+</div>
 
 
 
 <!-- Carousel -->
 
+<div class="container">
+<div class="row">
+<div class="col-sm-2"></div>
+  <div class="col-sm-8">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+		<li data-target="#myCarousel" data-slide-to="4"></li>
+		<li data-target="#myCarousel" data-slide-to="5"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox" style=" width:100%; height: 500px !important;">
+        <div class="item active">
+          <img src="Images/wolverine.jpg" alt="Wolverine">
+          <div class="carousel-caption">
+            <h3>Wolverine</h3>
+            <p>Original comic for sale in good to great condition</p>
+          </div>
+        </div>
+
+        <div class="item">
+          <img src="Images/archie.jpg" alt="Archie">
+          <div class="carousel-caption">
+            <h3>Archie</h3>
+            <p>Original Archie comics in good to great condition</p>
+          </div>
+        </div>
+      
+	  <div class="item">
+          <img src="Images/marvel.jpg" alt="Marvel">
+          <div class="carousel-caption">
+            <h3>Marvel</h3>
+            <p>Original Marvel comics in good to great condition</p>
+          </div>
+      </div>
+	  
+	  <div class="item">
+          <img src="Images/captainAmerica.jpg" alt="Captain America">
+          <div class="carousel-caption">
+            <h3>Captain America</h3>
+            <p>Original Captain America comics in good to great condition</p>
+          </div>
+      </div>
+	  
+	  <div class="item">
+          <img src="Images/madMagazine.jpg" alt="Mad Magazine">
+          <div class="carousel-caption">
+            <h3>Mad Magazine</h3>
+            <p>Original Mad Magazine in good to great condition</p>
+          </div>
+      </div>
+	  
+	  <div class="item">
+          <img src="Images/starWars.jpg" alt="Star Wars">
+          <div class="carousel-caption">
+            <h3>Star Wars</h3>
+            <p>Original Star Wars comics in good to great condition</p>
+          </div>
+      </div>
+	  
+	  <div class="item">
+          <img src="Images/silverSurfer.jpg" alt="Silver Surfer">
+          <div class="carousel-caption">
+            <h3>Silver Surfer</h3>
+            <p>Original Silver Surfer comics in good to great condition</p>
+          </div>
+      </div>
+     
+	</div>
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-circle-arrow-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+<hr>
+</div>
+</div>
 
   
-<!-- Table of Most Popular -->
+<!-- Table of Latest Releases and Most Popular -->
+
+
+<div class="container">
+<div class="row">
+<div class="sm-col-2"></div>
+<!-- Latest Releases -->
+<div class="sm-col-5">
+<h2>Latest Releases</h2>
+<p>A list of Latest Releases and Most Popular to be coming soon.</p>
 
 
 
-<!-- Footer -->
+
+
+</div>
+
+<!-- Most Popular -->
+
+
+</div>
+</div> <!-- end of latest releases and most popular container ==>
 
 
 
 <!-- Pagination -->
 
+<div class="container">
+    <ul class="pagination">
+    <li class="active"><a href="index.php">Home</a></li>
+    <li><a href="about.html">About</a></li>
+    <li><a href="gallery.html">Gallery</a></li>
+    <li><a href="shop.php">Shop</a></li>
+    <li><a href="sponsors.html">Sponsors</a></li>
+    <li><a href="contact.php">Contact Us</a></li>
+  </ul>
+</div>
+
+
+<!-- Footer -->
+
+<footer>
+	<p><a href="#">Site Map </a><a href="#">| Privacy Statement</a></p>
+	<p><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"> 2017 Comic Royale</span></p>
+
+</footer>
 
 </div> <!-- end of content container -->
 
