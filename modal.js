1f90f1104488d338b27761c377bede43a44a6644
IAN MOK <s3235507@student.rmit.edu.au>
2017-11-10 18:22:07 +1100

    some changes to the code but still not working

--- modal.js
+++ modal.js
@@ -3,7 +3,7 @@
 	var modal = document.getElementById('modalTest');
 
 	//  retrieves image to place into modal box Get the image and insert it inside the modal 
-	var img = document.getElementById('modalTest');
+	var img = document.getElementById('imgTest');
 	var modalImg = document.getElementById("archie");
 	var captionText = document.getElementById("caption"); //creates a caption for modal.
 		img.onclick = function(){
