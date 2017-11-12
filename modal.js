2791ecb6088788de03d7bc386a735074f445047d
lisaNeedsBracesToo <31608017+lisaNeedsBracesToo@users.noreply.github.com>
2017-11-13 09:23:17 +1100

    Modal.js syntax error fix - remove <script> tag from external .js file

--- modal.js
+++ modal.js
@@ -1,22 +1,23 @@
-<script>
-	//retreiving the modal box 
-	var modal = document.getElementById('modalTest');
+//retreiving the modal box 
+var modal = document.getElementById('modalTest');
 
-	//  retrieves image to place into modal box Get the image and insert it inside the modal 
-	var img = document.getElementById('imgTest');
-	var modalImg = document.getElementById("archie");
-	var captionText = document.getElementById("caption"); //creates a caption for modal.
-		img.onclick = function(){
-    		modal.style.display = "block";
-    		modalImg.src = this.src;
-    		captionText.innerHTML = this.alt;
-		}
+//  retrieves image to place into modal box Get the image and insert it inside the modal
+var img = document.getElementById('imgTest');
+var modalImg = document.getElementById("archie");
+var captionText = document.getElementById("caption"); //creates a caption for modal.
 
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+img.onclick = function ()
+{
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() { 
-  		modal.style.display = "none";
-	}
-</script>
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function ()
+{
+    modal.style.display = "none";
+}
