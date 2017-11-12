ea4a4a7b9d56478a93ca293f631e70e91d7cb973
lisaNeedsBracesToo <31608017+lisaNeedsBracesToo@users.noreply.github.com>
2017-11-13 10:21:40 +1100

    formatting fix - JS

--- modal.js
+++ modal.js
@@ -6,7 +6,7 @@ var img = document.getElementById('imgTest');
 var modalImg = document.getElementById("archie");
 var captionText = document.getElementById("caption"); //creates a caption for modal.
 
-img.onclick = function ()
+img.onclick = function()
 {
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -17,7 +17,7 @@ img.onclick = function ()
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function ()
+span.onclick = function()
 {
     modal.style.display = "none";
 }
