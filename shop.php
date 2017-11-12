188aadb587ddb3f4aa03000d969aeb2471a95cc9
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-13 10:58:17 +1100

    Update shop.php

--- shop.php
+++ shop.php
@@ -20,9 +20,9 @@
         <div class="row">
             <nav>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
+                    <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                 </ul>
             </nav>
 
@@ -32,15 +32,15 @@
                 <nav class="navbar">
                     <div class="container-fluid">
                         <div class="navbar-header">
-                            <a class="navbar-brand" href="#">Comic Royale</a>
+                            <a class="navbar-brand" href="index.php">Comic Royale</a>
                         </div>
                         <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Home</a></li>
+                            <li class="active"><a href="index.php">Home</a></li>
                             <li><a href="about.html">About</a></li>
                             <li><a href="gallery.html">Gallery</a></li>
-                            <li><a href="shop.html">Shop</a></li>
+                            <li><a href="shop.php">Shop</a></li>
                             <li><a href="sponsors.html">Sponsors</a></li>
-                            <li><a href="contact.html">Contact Us</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
                         </ul>
                     </div>
                 </nav>
