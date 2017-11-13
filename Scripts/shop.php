ec606fe2193018a8d150475feb41244216c6d8a4
lisaNeedsBracesToo <31608017+lisaNeedsBracesToo@users.noreply.github.com>
2017-11-13 11:04:42 +1100

    general folder cleanup: HTML in \views JS PHP in \scripts CSS in \css

---
+++ Scripts/shop.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CR Shop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="comicRoyale.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+
+
+<body>
+
+    <!-- Login and Registration Buttons/Links -->
+    <div class="container" name="content">
+        <div class="row">
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
+                    <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </nav>
+
+
+            <!-- NavBar -->
+            <div class="container">
+                <nav class="navbar">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="index.php">Comic Royale</a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li><a href="shop.php">Shop</a></li>
+                            <li><a href="sponsors.html">Sponsors</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div><!-- end of NavBarRow -->
+
+
+        <div class="container" name="aboutContainer">
+
+            <h1>COMIC ROYALE SHOP</h1>
+
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-6">
+                    <h1>Comic Royale Shop Coming Soon</h1>
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
+        </div><!-- end of aboutContainer container -->
+    </div><!-- end of content container -->
+
+</body>
+
+</html>
