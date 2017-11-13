eb6050198a4dae407bd47d5dd96648e8e9300776
lisaNeedsBracesToo <31608017+lisaNeedsBracesToo@users.noreply.github.com>
2017-11-13 16:17:56 +1100

    Commit shop page content finish the rest tonight

--- Views/shop.php
+++ Views/shop.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="comicRoyale.css">
+    <link rel="stylesheet" href="../CSS/comicRoyale.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -16,7 +16,7 @@
 <body>
 
     <!-- Login and Registration Buttons/Links -->
-    <div class="container">
+    <div class="container" name="content">
         <div class="row">
             <nav>
                 <ul class="nav navbar-nav navbar-right">
@@ -46,9 +46,9 @@
                 </nav>
             </div>
         </div><!-- end of NavBarRow -->
-
-
-        <div class="container">
+        <!-- Main Content Block -->
+        <!-- Coming Soon -->
+        <div class="container" name="aboutContainer">
 
             <h1>COMIC ROYALE SHOP</h1>
 
@@ -61,7 +61,52 @@
             </div>
         </div><!-- end of aboutContainer container -->
     </div><!-- end of content container -->
+    <!-- Actual Content -->
+    <section id="main-content-products">
 
+        <div id="product-links">
+            <p class="link-trail"><a href="index.php">Home</a> &#9658; <a href="shop.php" class="current-link">Shop</a></p>
+        </div>
+        <!-- Aside Content Block -->
+        <div class="aside-content-block">
+            <section class="product-article">
+
+                <!-- item 1 -->
+                <article>
+                    <img src="../images/archie.jpg" width="240" height="240" class="comicImgs" alt="Archie Comics" />
+                    <h2 class="product-article">Product 1</h2>
+                    <p>
+                        Archie<br /><br />
+                        <a href="../images/archie.jpg" class="lmore-sml">Learn More</a>
+                    </p>
+                </article>
+
+                <!-- item 2 -->
+                <article>
+                    <img src="../images/avengers.jpg" width="240" height="240" class="comicImgs" alt="Avengers Comics" />
+                    <h2 class="product-article">Product 2</h2>
+                    <p>
+                        Avengers<br /><br />
+                        <a href="../images/avengers.jpg" class="lmore-sml"> Learn More</a>
+                    </p>
+                </article>
+
+                <!-- item 3 -->
+                <article>
+                    <img src="../images/captainAmerica.jpg" width="240" height="240" class="comicImgs" alt="Captain America Comics" />
+                    <h2 class="product-article">Product 2</h2>
+                    <p>
+                        Captain America<br /><br />
+                        <a href="../images/captainAmerica.jpg" class="lmore-sml"> Learn More</a>
+                    </p>
+                </article>
+            </section>
+        </div>
+
+
+
+
+    </section>
 </body>
 
 </html>
