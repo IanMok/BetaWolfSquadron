a85b18baa53610a52fe6da9b0f2e01503ec994af
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-14 20:55:07 +1100

    Update login.php

--- Views/login.php
+++ Views/login.php
@@ -13,6 +13,38 @@
 <body>
 
     <div class="container">
+	<div class="row">
+<nav>
+    <ul class="nav navbar-nav navbar-right">
+	<li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
+	<li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+</nav>
+
+	
+<!-- NavBar -->	
+<div class="container">
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Comic Royale</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="gallery.html">Gallery</a></li>
+      <li><a href="shop.html">Shop</a></li>
+      <li><a href="sponsors.html">Sponsors</a></li>
+	  <li><a href="contact.html">Contact Us</a></li>
+    </ul>
+	</div>
+	</nav>
+</div>
+</div>
+
+
+<a href="index.php"><img src="Images/logoBanner.png" alt="Logo and Banner" /></a>
 
         <!-- login form; validate each field using regEx onSubmit-->
         <form class="form-horizontal" name=loginForm onsubmit=return(validateFields());>
