747153bda4c6e36b014358e7a3b8542780da3a74
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-15 19:09:03 +1100

    Update login.php

--- Views/login.php
+++ Views/login.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 
 <head>
-    <title>Comic Royale</title>
+    <title>CR Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="comicRoyale.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
