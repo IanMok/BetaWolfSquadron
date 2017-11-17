d99224f84719c47839bc2b0ed47583932d8d4916
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-17 13:46:09 +1100

    Update shop.php

--- Views/shop.php
+++ Views/shop.php
@@ -4,107 +4,77 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel="stylesheet" href="comicRoyale.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+           <link rel="stylesheet" href="comicRoyale.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
-
-
 <body>
+<div class="main">
+        <!-- Login and Registration Buttons/Links -->
+      <div class="container">
+	
 
-    <!-- Login and Registration Buttons/Links -->
-    <div class="container" name="content">
-        <div class="row">
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
-                    <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
-            </nav>
-
-
-            <!-- NavBar -->
-            <div class="container">
-                <nav class="navbar">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <a class="navbar-brand" href="index.php">Comic Royale</a>
-                        </div>
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="index.php">Home</a></li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="gallery.html">Gallery</a></li>
-                            <li><a href="shop.php">Shop</a></li>
-                            <li><a href="sponsors.html">Sponsors</a></li>
-                            <li><a href="contact.php">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div><!-- end of NavBarRow -->
-        <!-- Main Content Block -->
-        <!-- Coming Soon -->
-        <div class="container" name="aboutContainer">
-
-            <h1>COMIC ROYALE SHOP</h1>
-
+<div class="row">
+<nav>
+    <ul class="nav navbar-nav navbar-right">
+	<li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
+	<li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+</nav>
+</div>
+</div>
+<!-- NavBar -->	
+<div class="container">
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Comic Royale</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="gallery.html">Gallery</a></li>
+      <li><a href="shop.php">Shop</a></li>
+      <li><a href="sponsors.html">Sponsors</a></li>
+	  <li><a href="contact.php">Contact Us</a></li>
+    </ul>
+	</div>
+	</nav>
+</div>
+</div>
+        <!-- Search Bar -->
+        <div class="container">
             <div class="row">
-                <div class="col-sm-3"></div>
-                <div class="col-sm-6">
-                    <h1>Comic Royale Shop Coming Soon</h1>
-                </div>
-                <div class="col-sm-3"></div>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <label class="search">Search Comic Royale:</label>
+                        <input type="search" class="form-control" id="searchPlaceholder" placeholder="What would you like to look for?">
+                    </div>
+                </form>
             </div>
-        </div><!-- end of aboutContainer container -->
-    </div><!-- end of content container -->
-    <!-- Actual Content -->
-    <section id="main-content-products">
-
-        <div id="product-links">
-            <p class="link-trail"><a href="index.php">Home</a> &#9658; <a href="shop.php" class="current-link">Shop</a></p>
+        </div><!-- end of Search bar container -->
+		<br>
+        <!-- Banner with logo -->
+        <div class="container">
+            <a href="index.php"><img src="Images/logoBanner.png" alt="Comic Royale Logo & Banner"/></a>
         </div>
-        <!-- Aside Content Block -->
-        <div class="aside-content-block">
-            <section class="product-article">
+<br>
+<div class="container" >
 
-                <!-- item 1 -->
-                <article>
-                    <img src="../images/archie.jpg" width="240" height="240" class="comicImgs" alt="Archie Comics" />
-                    <h2 class="product-article">Product 1</h2>
-                    <p>
-                        Archie<br /><br />
-                        <a href="../images/archie.jpg" class="lmore-sml">Learn More</a>
-                    </p>
-                </article>
+<h1>COMIC ROYALE SHOP</h1>
 
-                <!-- item 2 -->
-                <article>
-                    <img src="../images/avengers.jpg" width="240" height="240" class="comicImgs" alt="Avengers Comics" />
-                    <h2 class="product-article">Product 2</h2>
-                    <p>
-                        Avengers<br /><br />
-                        <a href="../images/avengers.jpg" class="lmore-sml"> Learn More</a>
-                    </p>
-                </article>
+<div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6">
+	<h1>Comic Royale Shop Coming Soon</h1>
+	</div>
+    <div class="col-sm-3"></div>
+</div>
+</div><!-- end of shopContainer container -->
+</div><!-- end of content container -->
 
-                <!-- item 3 -->
-                <article>
-                    <img src="../images/captainAmerica.jpg" width="240" height="240" class="comicImgs" alt="Captain America Comics" />
-                    <h2 class="product-article">Product 2</h2>
-                    <p>
-                        Captain America<br /><br />
-                        <a href="../images/captainAmerica.jpg" class="lmore-sml"> Learn More</a>
-                    </p>
-                </article>
-            </section>
-        </div>
-
-
-
-
-    </section>
 </body>
 
 </html>
