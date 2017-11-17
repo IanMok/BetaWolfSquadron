b39c1dfbcc0ed5f1a35da62c8cad258ea7b8f6b2
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-17 13:39:53 +1100

    Update index.php

--- Views/index.php
+++ Views/index.php
@@ -12,7 +12,8 @@
 <body>
 <div class="main">
         <!-- Login and Registration Buttons/Links -->
-      <div class="row">
+     <div class="container">
+	 <div class="row">
 <nav>
     <ul class="nav navbar-nav navbar-right">
 	<li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
@@ -20,6 +21,8 @@
 	<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
 </nav>
+</div>
+</div>
 <!-- NavBar -->	
 <div class="container">
 <nav class="navbar">
@@ -31,9 +34,9 @@
       <li class="active"><a href="index.php">Home</a></li>
       <li><a href="about.html">About</a></li>
       <li><a href="gallery.html">Gallery</a></li>
-      <li><a href="shop.html">Shop</a></li>
+      <li><a href="shop.php">Shop</a></li>
       <li><a href="sponsors.html">Sponsors</a></li>
-	  <li><a href="contact.html">Contact Us</a></li>
+	  <li><a href="contact.php">Contact Us</a></li>
     </ul>
 	</div>
 	</nav>
