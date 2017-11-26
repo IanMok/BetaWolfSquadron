38b88cd3928a992f8918dff240091f10942b0e6c
WooJesh <31717648+WooJesh@users.noreply.github.com>
2017-11-27 10:23:46 +1100

    Update contact.php

--- Views/contact.php
+++ Views/contact.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <title>Contact CR</title>
+    <title>Comic Royale</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -10,17 +10,20 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="main">
-        <!-- Login and Registration Buttons/Links -->
-      <div class="container"><div class="row">
+      <!-- Login and Registration Buttons/Links -->
+     <div class="container">
+	 <div class="row">
+	 <div class="container">
 <nav>
     <ul class="nav navbar-nav navbar-right">
 	<li><a href="shop.php"><span class="glyphicon glyphicon-shopping-cart"></span> Shop</a></li>
 	<li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
 	<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
-</nav></div>
+</nav>
 </div>
+
+
 <!-- NavBar -->	
 <div class="container">
 <nav class="navbar">
@@ -39,8 +42,13 @@
 	</div>
 	</nav>
 </div>
+
 </div>
+</div>
+
         <!-- Search Bar -->
+		
+		<center>
         <div class="container">
             <div class="row">
                 <form class="form-inline">
@@ -57,6 +65,7 @@
             <a href="index.php"><img src="Images/logoBanner.png" alt="Comic Royale Logo & Banner"/></a>
         </div>
 <br>
+<div class="container">
 <h1>Contact Comic Royale</h1>
 
 
@@ -76,8 +85,27 @@
 </div>
  <button type="submit" class="btn btn-default">Submit</button>
  </form>
- 
-</div><!-- end of contentContainer -->
-
+ </div>
+<br>
+<br>
+    <!-- Pagination -->
+        <div class="container">
+            <ul class="pagination">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="sponsors.html">Sponsors</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </div>
+<br>
+<!-- Footer -->
+        <footer>
+            <p><a href="siteMap.html">Site Map </a>
+			<a href="privacy.html">| Privacy Statement</a></p>
+            <p><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"> 2017 Comic Royale</span></p>
+        </footer>
+</center>
 </body>
 </html>
